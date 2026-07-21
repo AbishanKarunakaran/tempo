545e3825c499d56cc98743a88ed44a89ece2cbc6
Abishan Karunakaran <karunakaranabishan@gmail.com>
2026-07-21 15:17:22 +0530

    Update CORS origin to a specific URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // app.use(cors({ origin: true, credentials: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://greenjustice2026.netlify.app",
   credentials: true,
 }));
 app.use(express.json());
